33e0aaba93ccfe756891c14531e709c742f5f881
Alex <ss47305@gmail.com>
2020-11-23 23:55:59 +0300

    client-server communication [WIP]

--- app/src/main/java/com/eltech/snc/MainActivity.java
+++ app/src/main/java/com/eltech/snc/MainActivity.java
@@ -1,19 +1,35 @@
 package com.eltech.snc;
 
 import android.annotation.SuppressLint;
+import android.app.Application;
 import android.os.Bundle;
 import android.view.View;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import com.eltech.snc.utils.ServerApi;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.android.material.navigation.NavigationView;
+import org.json.JSONException;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
 
 public class MainActivity extends AppCompatActivity {
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
     private AppBarConfiguration mAppBarConfiguration;
 
     @SuppressLint("ClickableViewAccessibility")
@@ -43,5 +59,27 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    private void registerUser() {
+        UUID userName = UUID.randomUUID();
+        try {
+            Integer userId = ServerApi.getInstance().createUser(userName.toString(), this);
+            FileOutputStream fileOutputStream = new FileOutputStream("/settings");
+            fileOutputStream.write(OBJECT_MAPPER.writeValueAsBytes(userName));
+            fileOutputStream.close();
+        } catch (JSONException | InterruptedException | ExecutionException | TimeoutException | IOException e) {
+            e.printStackTrace();
+            AlertDialog.Builder errorDialog = new AlertDialog.Builder(this)
+                    .setMessage("Error creating user. Closing app ...")
+                    .setPositiveButton("OK", (dialog, which) -> {
+                        this.finish();
+                        System.exit(-1);
+                    });
+            errorDialog.create();
+            DialogFragment newFragment = new DialogFragment();
+            newFragment.show(getSupportFragmentManager(), "missiles");
+
+        }
     }
 }
--- app/src/main/java/com/eltech/snc/utils/ServerApi.java
+++ app/src/main/java/com/eltech/snc/utils/ServerApi.java
@@ -1,17 +1,50 @@
 package com.eltech.snc.utils;
 
 import android.content.Context;
+import android.graphics.Path;
+import android.os.Build;
+import androidx.annotation.RequiresApi;
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.RequestFuture;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.eltech.snc.server.jpa.entity.MazeEntity;
+import com.eltech.snc.server.jpa.entity.RollingBallEntity;
+import com.eltech.snc.server.jpa.entity.StencilEntity;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ser.std.JsonValueSerializer;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 public final class ServerApi {
     private static volatile ServerApi instance;
     private static final String CREATE_USER_URL = "/createUser";
+    private static final String FIND_USER_URL = "/findUser";
     private static final String SAVE_IMAGE_RESULT = "/saveStencilStatistic";
     private static final String SAVE_MAZE_RESULT = "/saveMazeStatistic";
     private static final String SAVE_BALL_RESULT = "/saveRollingBallStatistic";
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
-    private ServerApi() {}
+    private final String serverAddress = "";
+
+    private Integer userId;
+
+    private ServerApi() {
+    }
 
     public static ServerApi getInstance() {
         if (instance == null) {
@@ -25,24 +58,77 @@ public final class ServerApi {
         return instance;
     }
 
-    public Integer createUser(final String userName, final Context context) {
+    public Integer createUser(final String userName, final Context context) throws JSONException, InterruptedException, ExecutionException, TimeoutException {
+        RequestFuture<JSONObject> future = RequestFuture.newFuture();
         RequestQueue requestQueue = Volley.newRequestQueue(context);
-        throw new UnsupportedOperationException();
+        JSONObject payload = new JSONObject().accumulate("userName", userName);
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, serverAddress + CREATE_USER_URL,
+                                                          payload, future, future);
+        requestQueue.add(request);
+        Integer id = (Integer) future.get(30, TimeUnit.SECONDS).get("id");
+        userId = id;
+        return id;
     }
 
-    public Integer getUserId(final String userName) {
-        throw new UnsupportedOperationException();
+    public Integer getUserId(final String userName, final Context context) throws InterruptedException, ExecutionException, TimeoutException {
+        RequestFuture<String> future = RequestFuture.newFuture();
+        RequestQueue requestQueue = Volley.newRequestQueue(context);
+        StringRequest request = new StringRequest(Request.Method.GET, serverAddress + FIND_USER_URL + "?" + userName,
+                                                  future, future);
+        requestQueue.add(request);
+        Integer id = Integer.valueOf(future.get(30, TimeUnit.SECONDS));
+        userId = id;
+        return id;
     }
 
-    public void sendImageModuleResult(final float result, final Context context) {
-        throw new UnsupportedOperationException();
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void sendImageModuleResult(final double result, final Context context) throws JsonProcessingException, JSONException {
+        if (userId == null) throw new RuntimeException("Unknown user");
+
+        StencilEntity stencilEntity = new StencilEntity();
+        stencilEntity.setUserId(userId);
+        stencilEntity.setResult(result);
+        stencilEntity.setDate(LocalDateTime.now());
+
+        RequestQueue requestQueue = Volley.newRequestQueue(context);
+        JSONObject payload = new JSONObject(OBJECT_MAPPER.writeValueAsString(stencilEntity));
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, serverAddress + SAVE_IMAGE_RESULT,
+                                                          payload, response -> {}, null);
+
+        requestQueue.add(request);
     }
 
-    public void sendMazeModuleResult(final float result, final Context context) {
-        throw new UnsupportedOperationException();
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void sendMazeModuleResult(final double result, final Context context) throws JsonProcessingException, JSONException {
+        if (userId == null) throw new RuntimeException("Unknown user");
+
+        MazeEntity mazeEntity = new MazeEntity();
+        mazeEntity.setUserId(userId);
+        mazeEntity.setResult(result);
+        mazeEntity.setDate(LocalDateTime.now());
+
+        RequestQueue requestQueue = Volley.newRequestQueue(context);
+        JSONObject payload = new JSONObject(OBJECT_MAPPER.writeValueAsString(mazeEntity));
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, serverAddress + SAVE_MAZE_RESULT,
+                                                          payload, response -> {}, null);
+
+        requestQueue.add(request);
     }
 
-    public void sendBallModuleResult(final float result, final Context context) {
-        throw new UnsupportedOperationException();
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void sendBallModuleResult(final double result, final Context context) throws JsonProcessingException, JSONException {
+        if (userId == null) throw new RuntimeException("Unknown user");
+
+        RollingBallEntity rollingBallEntity = new RollingBallEntity();
+        rollingBallEntity.setUserId(userId);
+        rollingBallEntity.setResult(result);
+        rollingBallEntity.setDate(LocalDateTime.now());
+
+        RequestQueue requestQueue = Volley.newRequestQueue(context);
+        JSONObject payload = new JSONObject(OBJECT_MAPPER.writeValueAsString(rollingBallEntity));
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, serverAddress + SAVE_BALL_RESULT,
+                                                          payload, response -> {}, null);
+
+        requestQueue.add(request);
     }
 }
